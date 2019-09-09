c6dab7c22069b7d4f1551b999155bbc8141221e8
RoseliNogueira <55070066+RoseliNogueira@users.noreply.github.com>
2019-09-08 22:22:41 -0400

    Projet_Galerie_Ajoute

---
+++ Projet_Galerie_OB/GalerieArtSGIWin/Artiste.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GalerieArtSGIWin
+{
+    public class Artiste : Personne
+    {
+        private string ID = "";
+
+        public string ArtistID
+        {
+            get
+            {
+                return ID;
+            }
+            set
+            {
+                ID = value;
+            }
+        }
+
+        public Artiste(string First, string Last, string artID)
+            : base(First, Last)
+        {
+            ID = artID;
+        }
+
+        public override string ToString()
+        {
+            string artistInformation = "\nName: " + base.FistName + " " + base.LastName + "\nArtistID: " + ID + "\n\n";
+
+            return artistInformation;
+        }
+    }
+}
